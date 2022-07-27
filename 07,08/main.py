21cfd09ff9e8f369c198396b6c6ab8efddfb47d9
18296 <87044961+18296@users.noreply.github.com>
2022-07-27 18:07:26 +1200

    Add files via upload
    Updated Map:
    - Map class allows for class attributes
    - Dictionary allows multiple types of tiles to potentially have separate interactions with player
    
    collision_test Function / movement Function update:
    - Checks for any tiles the player may be colliding with
    - Stops player from glitching into tile by setting them back if the player collides with a tile.

---
+++ 07,08/main.py
@@ -0,0 +1,163 @@
+import pygame, sys
+from pygame import *
+from math import *
+
+init()
+
+# window
+screendim = [800, 600]
+
+screen = display.set_mode((screendim[0], screendim[1]))
+
+# colours
+white = (255, 255, 255)
+red = (255, 0, 0)
+turquoise = (49, 129, 138)
+pink = (255, 105, 180)
+
+# Map
+# https://pythonprogramming.altervista.org/platform-game-in-detail-part-1/?doing_wp_cron=1655762207.1499760150909423828125
+# t = solid tile, h = hole(death)
+map1 = """tttttttttttttttttttt
+t                  t
+t                  t
+t                  t
+t        tt        t
+t                  t
+t      tttttt      t
+t                  t
+t                  t
+t                  t
+t         h        t
+t     tttttttt     t
+t                  t
+t     tt    tt     t
+tttttttttttttttttttt
+"""
+
+
+class Map:
+    def __init__(self, m, rect_dim):  # self, map, graphical overlay, tiles in x axis, tiles in y axis
+        self.tile_list = []
+        self.tile_dict = {'t': [], 'h': []}
+        self.m = m.splitlines()
+
+        for y, line in enumerate(self.m):
+            for x, c in enumerate(line):
+
+                try:
+                    self.tile_dict[c].append(Rect(x * rect_dim[0], y * rect_dim[1], rect_dim[0], rect_dim[1]))
+
+                except KeyError:
+                    pass
+
+                if c == "t":
+                    self.tile_list.append(Rect(x * rect_dim[0], y * rect_dim[1], rect_dim[0], rect_dim[1]))
+
+    def draw_tiles(self):
+        for key in self.tile_dict:
+            for item in self.tile_dict[key]:
+                draw.rect(screen, white, item)
+
+    def collision_test(self, rect):
+        collisions = {'t': [], 'h': []}
+        for key in self.tile_dict:
+            for tile in self.tile_dict[key]:
+                if rect.colliderect(tile):
+                    collisions[key].append(tile)
+
+        return collisions
+
+
+map1 = Map(map1, [40, 40])
+
+
+# Player
+
+
+class Player:
+    def __init__(self, dim):
+        self.dim = dim
+        self.rect = Rect(50, 50, dim[0], dim[1])
+        self.colour = pink
+        self.left, self.right, self.up, self.down = False, False, False, False
+        self.facing = pi
+        self.movement = [0, 0]
+
+    def move(self, m):  # movement = [x,y]
+        self.movement = [0, 0]
+        if self.right:
+            self.movement[0] += 5
+        if self.left:
+            self.movement[0] -= 5
+        if self.up:
+            self.movement[1] -= 5
+        if self.down:
+            self.movement[1] += 5
+
+        self.rect.x += self.movement[0]
+        collisions = m.collision_test(self.rect)
+        for list in collisions.values():
+            for tile in list:
+                if self.movement[0] > 0:
+                    self.rect.right = tile.left
+                if self.movement[0] < 0:
+                    self.rect.left = tile.right
+
+        self.rect.y += self.movement[1]
+        collisions = m.collision_test(self.rect)
+        for list in collisions.values():
+            for tile in list:
+                if self.movement[1] > 0:
+                    self.rect.bottom = tile.top
+                if self.movement[1] < 0:
+                    self.rect.top = tile.bottom
+
+    def draw_player(self):
+        draw.rect(screen, self.colour, self.rect)
+
+
+
+player1 = Player([40, 40])
+
+# Main Loop
+x = 0
+game_loop = True
+while game_loop:
+    # FPS
+    fps = 60
+    time.Clock().tick(fps)
+    screen.fill(turquoise)
+
+    # Human Interaction
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            game_loop = False
+
+        if event.type == KEYDOWN:
+            if event.key == K_RIGHT:
+                player1.right = True
+            if event.key == K_LEFT:
+                player1.left = True
+            if event.key == K_DOWN:
+                player1.down = True
+            if event.key == K_UP:
+                player1.up = True
+
+
+        if event.type == KEYUP:
+            if event.key == K_RIGHT:
+                player1.right = False
+            if event.key == K_LEFT:
+                player1.left = False
+            if event.key == K_DOWN:
+                player1.down = False
+            if event.key == K_UP:
+                player1.up = False
+
+    player1.move(map1)
+    player1.draw_player()
+
+    map1.draw_tiles()
+
+    display.update()
